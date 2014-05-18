144311f4024cc8caa74e0666cebb9b33fdaf557d
John Polling <john@theled.co.uk>
2014-05-18 07:40:11 +0100

    Building the Results differently

--- app/services/rapidfire/question_group_results.rb
+++ app/services/rapidfire/question_group_results.rb
@@ -8,7 +8,27 @@ module Rapidfire
     # 2. question text
     # 3. if aggregatable, return each option with value
     # 4. else return an array of all the answers given
-    def extract(start_date, end_date)
+    #def extract(start_date, end_date, users)
+    def extract(answer_groups)
+      @question_group.questions.collect do |question|
+        results =
+          case question
+          when Rapidfire::Questions::Select, Rapidfire::Questions::Radio,
+            Rapidfire::Questions::Checkbox
+            answers = get_answer_groups_answers(answer_groups, question).map { |text| text.split(',') }.flatten
+            answers.inject(Hash.new(0)) { |total, e| total[e] += 1; total }
+
+          when Rapidfire::Questions::Short, Rapidfire::Questions::Date,
+            Rapidfire::Questions::Long, Rapidfire::Questions::Numeric
+            get_answer_groups_answers(answer_groups, question)
+          end
+
+        QuestionResult.new(question: question, results: results)
+      end
+    end
+  
+  
+    def extract_old(start_date, end_date)
       @question_group.questions.collect do |question|
         results =
           case question
@@ -20,6 +40,7 @@ module Rapidfire
               question.answers.submitted_between(start_date, end_date).length
               answers = question.answers.submitted_between(start_date, end_date).map(&:answer_text).map { |text| text.split(',') }.flatten
             end
+            
             answers.inject(Hash.new(0)) { |total, e| total[e] += 1; total }
 
           when Rapidfire::Questions::Short, Rapidfire::Questions::Date,
@@ -33,6 +54,18 @@ module Rapidfire
 
         QuestionResult.new(question: question, results: results)
       end
+    end
+  
+  
+    private
+  
+    def get_answer_groups_answers(answer_groups, question)
+      found_answers = []
+      answer_groups.each do |answer_group|
+        found_answers << answer_group.answers.select {|a| a.question_id == question.id }
+      end
+    
+      found_answers.map { |a| a.first.answer_text }
     end
   end
 end
